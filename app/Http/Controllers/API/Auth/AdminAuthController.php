6e6c0edb06736735dfa34ff85aec2107c03d713c
billah1 <billah9578@gmail.com>
2024-02-01 15:57:24 +0600

    multi auth

---
+++ app/Http/Controllers/API/Auth/AdminAuthController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\API\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\Admin;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AdminAuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $admin = Admin::where('email', $request->email)->first();
+
+        if (!$admin || !Hash::check($request->password, $admin->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        return response()->json([
+            'status'  => true,
+            'message' =>'you have sucessfully admin login .',
+            'admin'   => $admin,
+            'token'   => $admin->createToken('mobile', ['role:admin'])->plainTextToken
+        ]);
+
+    }
+}
---
+++ app/Http/Controllers/API/Auth/UserAuthController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\API\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class UserAuthController extends Controller
+{
+
+    public function register(Request $request)
+    {
+        // Validate the request data
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string|min:8',
+        ]);
+
+        // Create a new user
+        $user = User::create([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => bcrypt($request->input('password')),
+        ]);
+
+        // You may want to customize the response based on your needs
+        return response()->json(['message' => 'User registered successfully', 'user' => $user]);
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        return response()->json([
+            'status'  => true,
+            'message' =>'you have sucessfully user login .',
+            'customer' => $user,
+            'token' => $user->createToken('mobile', ['role:user'])->plainTextToken
+        ]);
+
+    }
+    public function logout(Request $request)
+    {
+        $user = Auth::guard('api')->user();
+        $user->api_token = null;
+        $user->save();
+
+        return response()->json(['message' => 'You are successfully logged out'], 200);
+    }
+}
